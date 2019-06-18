296fbb27ee8a127831047330932f19f60f4aaa27
PieSignal <insiro@naver.com>
2019-06-18 19:33:03 +0900

    overwach parser

---
+++ toornamentOW.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+link = 'https://www.toornament.com/games/overwatch'
+tag = '#tournament-game-list-tab > section > div'
+req = requests.get(link)
+html = req.text
+soup = BeautifulSoup(html, 'html.parser')
+i = 0
+lists = list()
+with open('toornament.json', 'w') as OWoutput:
+    div = soup.select(tag)
+    for i in range(2, 5):
+        j = 0
+        for label in div[i].select('div > a > div'):
+            j += 1
+            if j == 2:
+                name = label.select('div')[0].text
+            elif j == 3:
+                k = 1
+                end = None
+                for timetag in label.select('div > time'):
+                    if k == 1:
+                        start = ''.join(timetag['datetime'].split('-'))
+                    else:
+                        end = ''.join(timetag['datetime'].split('-'))
+                    k = -k
+            elif j == 5:
+                lists.append({'start': start, 'end': end, 'name': name})
+                j = 0
+    OWoutput.write(json.dumps(lists))
