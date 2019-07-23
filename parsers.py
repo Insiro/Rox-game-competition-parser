1ca946e492f0bec4198e16242375906c8cddec7e
piesignal <insiro@naver.com>
2019-07-23 16:59:08 +0900

    uploader

--- parsers.py
+++ parsers.py
@@ -1,11 +1,12 @@
 import pubg_esports_gamepedia
 import lol_gamepedia
 import toornamentOW
-
+import LOL_uploader
 print('pubg gamepedia parsing')
 pubg_esports_gamepedia.parse()
 print('lol gamepedia parsing')
 lol_gamepedia.parse()
 print('toornament OW parsing')
 toornamentOW.parse()
-print('end')
+print('upload')
+LOL_uploader.upload()
