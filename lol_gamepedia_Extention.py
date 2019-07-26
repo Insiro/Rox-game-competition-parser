8bf14dd406b89cf0a72ff17755a9d6fb7aa56553
piesignal <insiro@naver.com>
2019-07-26 17:15:58 +0900

    .

--- lol_gamepedia_Extention.py
+++ lol_gamepedia_Extention.py
@@ -17,11 +17,11 @@ def parse(link):
     req = requests.get(link)
     html = req.text
     soup = BeautifulSoup(html, 'html.parser')
-    heads = {'Organizer', 'Prize Pool', 'Region', 'Event Type',
+    heads = {'Organizer', 'Prize Pool', 'Prize', 'Region', 'Event Type',
              'Links', 'Streams', 'Schedule'  # , 'Sponsor(s)'
              }
     tag = '#infoboxTournament > tbody > tr > td'
-    data = {'Organizer': None, 'Number of Teams': None, 'Tier': None,'Prize': None, 'Region': None,
+    data = {'Organizer': None, 'Number of Teams': None, 'Tier': None, 'Prize Pool': None, 'Region': None,
             'Type': None, 'Links': None, 'Streams': None, 'Schedule': None  # ,'Sponsors': None
             }
     for block in soup.select(tag):
